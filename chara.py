26ef553243b0dff0988c5f9fc75a43ead978835a
Sourcery AI <>
2023-09-27 10:38:10 +0000

    'Refactored by Sourcery'

--- chara.py
+++ chara.py
@@ -10,32 +10,23 @@ with open(join(CONFIG_PATH,'charaname.json'), 'r', encoding='UTF-8') as f:
         charaname = json.load(f)
 
 async def check_chara(name:str):
-    try:
-        for i in charaname:
-            if name in i:
-                return i[0]
-        return None
-    except Exception as e:
-        logger.error(e)
-        return None
+        try:
+                return next((i[0] for i in charaname if name in i), None)
+        except Exception as e:
+            logger.error(e)
+            return None
 
 
 async def check_name(name:str):
-    try:
-        for i in characters:
-            if i["name"] == name:
-                return i
-        return None
-    except Exception as e:
-        logger.error(e)
-        return None
+        try:
+                return next((i for i in characters if i["name"] == name), None)
+        except Exception as e:
+            logger.error(e)
+            return None
 
 async def all_chara(name:str):
-    try:
-        for i in charaname:
-            if name in i:
-                return [i[0],",".join(i)]
-        return None
-    except Exception as e:
-        logger.error(e)
-        return None
+        try:
+                return next(([i[0],",".join(i)] for i in charaname if name in i), None)
+        except Exception as e:
+            logger.error(e)
+            return None
